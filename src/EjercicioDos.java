331af764bba3ae9c02e9907e7d9674a982a43afb
Walther Ortiz <wsortiz89@hotmail.com>
2024-10-18 19:08:25 -0500

    Actualizacion2

--- src/EjercicioDos.java
+++ src/EjercicioDos.java
@@ -8,20 +8,20 @@ public class EjercicioDos {
         for (int i = 1; i < 6; i++) {
             System.out.println(i);
         }
-        System.out.println("for");
+            System.out.println("for");
         for (int i = 0; i < 6; i++) {
             System.out.println(i);
         }
 
-        System.out.println("foreach:");
+            System.out.println("foreach:");
         for (Integer number : numbers) {
             System.out.println(number);
         }
-        System.out.println("While:");
+            System.out.println("While:");
         int index = numbers.size() - 1;
         while (index >= 0) {
-            System.out.println(numbers.get(index));
-            index--; // Decrementar el índice
+            System.out.println(numbers.get(index));//accede al elemento en la posición index
+            index--;
         }
     }
 }
